a8fed0a0f63a9aba35d3c84177023314e5e5af77
Callan White <callan.white@neutronvr.com>
2016-08-11 08:18:58 +0100

    push

---
+++ AspRouting/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspRouting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			IWebHost host = new WebHostBuilder()
+				.UseStartup<Startup>()
+				.UseKestrel()
+				.UseUrls(new[] { "http://localhost:1234" })
+				.Build();
+
+			host.Run();
+        }
+    }
+
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddRouting();
+		}
+
+		public void Configure(IApplicationBuilder app)
+		{
+			RouteHandler testRouteHandler = new RouteHandler(ctx =>
+			{
+				return ctx.Response.WriteAsync(string.Empty);
+			});
+
+			RouteBuilder routeBuilder = new RouteBuilder(app, testRouteHandler);
+			routeBuilder.MapGet("", ctx =>
+			{
+				return ctx.Response.WriteAsync(System.Guid.NewGuid().ToString());
+			});
+			routeBuilder.MapPost("", ctx =>
+			{
+				return ctx.Response.WriteAsync(System.Guid.NewGuid().ToString());
+			});
+
+			app.UseRouter(routeBuilder.Build());
+
+		}
+	}
+}
---
+++ NancyRouting/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Nancy;
+using Nancy.Owin;
+
+namespace NancyRouting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			IWebHost host = new WebHostBuilder()
+				.UseKestrel()
+				.Configure(conf =>
+				{
+					conf.UseOwin(o => o.UseNancy());
+				})
+				.UseUrls(new[] { "http://localhost:1234" })
+				.Build();
+
+			host.Run();
+        }
+    }
+
+	public class TestRoute : NancyModule
+	{
+		public TestRoute()
+		{
+			Get("/", _ =>
+			{
+				return System.Guid.NewGuid().ToString();
+			});
+
+			Post("/", _ =>
+			{
+				return System.Guid.NewGuid().ToString();
+			});
+		}
+	}
+}
